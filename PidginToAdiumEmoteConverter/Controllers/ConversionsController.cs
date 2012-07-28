9ccb4cf40732a4759530af9d2ed539e2d8d9fe3b
HMW <sweetnhappy+git@gmail.com>
2012-07-28 15:37:36 -0700

    Pidgin to Adium Emote Converter initial version

---
+++ PidginToAdiumEmoteConverter/Controllers/ConversionsController.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using PidginToAdiumEmoteConverter.Models;
+
+namespace PidginToAdiumEmoteConverter.Controllers
+{
+    public class ConversionsController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(ConversionInputReadModel input)
+        {
+            ConversionOutputReadModel output = new ConversionOutputReadModel(input);
+
+            return View("Convert", output);
+        }
+    }
+}
